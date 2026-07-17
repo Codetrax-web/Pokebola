62394356a5ed56d2897a2e61659cf90fccb00570
DAMIAN_CV <137531039+Codetrax-web@users.noreply.github.com>
2026-07-17 00:21:53 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,21 +1,114 @@
+// Elementos principales
+
 const pokeball = document.getElementById("pokeball");
 
+const openBtn = document.getElementById("openBtn");
 
-let abierta = false;
+const resetBtn = document.getElementById("resetBtn");
 
 
+const music = document.getElementById("music");
+
+const openSound = document.getElementById("openSound");
+
+
+
+// Estado
+
+let abierto = false;
+
+
+
+// Función abrir regalo
+
+function abrirPokebola(){
+
+
+    if(abierto) return;
+
+
+    abierto = true;
+
+
+    // activar animación CSS
+
+    pokeball.classList.add("open");
+
+
+
+    // sonido de apertura
+
+    openSound.currentTime = 0;
+
+    openSound.play()
+    .catch(()=>{
+
+        console.log("El navegador bloqueó el sonido");
+
+    });
+
+
+
+    // música
+
+    music.volume = 0.4;
+
+    music.play()
+    .catch(()=>{
+
+        console.log("La música necesita interacción");
+
+    });
+
+
+
+}
+
+
+
+// Botón abrir
+
+openBtn.addEventListener("click",()=>{
+
+    abrirPokebola();
+
+});
+
+
+
+// También abre tocando la Pokébola
 
 pokeball.addEventListener("click",()=>{
 
+    abrirPokebola();
 
-if(abierta) return;
-
-
-abierta=true;
+});
 
 
 
-pokeball.classList.add("open");
+
+// Reiniciar
+
+resetBtn.addEventListener("click",()=>{
+
+
+    abierto=false;
+
+
+    pokeball.classList.remove("open");
+
+
+    // detener sonidos
+
+    openSound.pause();
+
+    openSound.currentTime=0;
+
+
+
+    music.pause();
+
+    music.currentTime=0;
 
 
 
